4c345724f7fb5e56f42ac67af5cbf9aa3ed926ea
Usama Subhani <reachusama@protonmail.com>
2020-10-24 14:29:01 +0500

    Create 'Hello World' lambda function

---
+++ functions/graphql/index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require('apollo-server-lambda')
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello World!!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+
+    playground: true,
+    introspection: true
+})
+
+exports.handler = server.createHandler()
