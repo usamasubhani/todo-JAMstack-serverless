36d6b39084d37d0af56fb0641e84c04216123636
Usama Subhani <reachusama@protonmail.com>
2020-10-24 17:11:56 +0500

    Implement apollo-server with in-memory cache

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,4 +1,17 @@
 const React = require("react");
+const { ApolloClient, HttpLink, ApolloProvider, InMemoryCache } = require("@apollo/client")
 const wrapRootElement = require("./wrap-root-element");
 
-exports.wrapRootElement = wrapRootElement;
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: "https://todo-jamstack-serverless.netlify.app/.netlify/functions/graphql"
+    })
+})
+
+exports.wrapRootElement = ({ element }) => (
+    <ApolloProvider client={client}>
+        {wrapRootElement({element})}
+    </ApolloProvider>
+)
+// exports.wrapRootElement = wrapRootElement;
--- packages/www/src/pages/dashboard.js
+++ packages/www/src/pages/dashboard.js
@@ -1,5 +1,33 @@
 import React, { useReducer, useRef } from 'react';
 import { Button, Container, Heading, Flex, Label, Input, Checkbox } from "theme-ui"
+import { gql, useMutation, useQuery } from "@apollo/client"
+
+const ADD_TODO = gql`
+    mutation AddTodo($text: String!) {
+        addTodo(text: $text) {
+            id
+        }
+    }
+`
+
+const GET_TODOS = gql`
+    query GetTodos {
+        todos {
+            id
+            text
+            done
+        }
+    }
+`
+
+const UPDATE_TODO_DONE = gql`
+    mutation UpdateTodoDone($id: ID!){
+        updateTodoDone(id: $id) {
+            text
+            done
+        }
+    }
+`
 
 const todosReducer = (state, action) => {
     switch (action.type) {
@@ -19,15 +47,20 @@ const todosReducer = (state, action) => {
 export default () => {
     const [todos, dispatch] = useReducer(todosReducer, [])
     const inputRef = useRef("")
-
+    const [addTodo] = useMutation(ADD_TODO)
+    const [updateTodoDone] = useMutation(UPDATE_TODO_DONE)
+    const { loading, error, data, refetch } = useQuery(GET_TODOS)
+    
     return (
         <Container>
             <Flex
                 as="form"
-                onSubmit={e => {
+                onSubmit={async e => {
                     e.preventDefault()
-                    console.log(inputRef.current.value)
-                    dispatch({ type: "addTodo", payload: inputRef.current.value })
+                    await addTodo({ variables: { text: inputRef.current.value } })
+                    inputRef.current.value = ""
+                    await refetch()
+                    // dispatch({ type: "addTodo", payload: inputRef.current.value })
                 }}
             >
                 <Label sx={{ display: "flex" }}>
@@ -39,20 +72,26 @@ export default () => {
             </Flex>
 
             <Flex>
-                <ul>
-                    {todos.map((todo, i) => (
-                        <Flex
-                            as="li"
-                            key={i}
-                            onClick={() => {
-                                dispatch({ type: "toggleTodo", payload: 0 })
-                            }}
-                        >
-                            <Checkbox checked={todo.done} />
-                            <span>{todo.value}</span>
-                        </Flex>
-                    ))}
-                </ul>
+                {loading ? <div>Loading...</div> : null}
+                {error ? <div>{error.message}</div> : null}
+                {!loading && !error && (
+                    <ul>
+                        {data.todos.map((todo) => (
+                            <Flex
+                                as="li"
+                                key={todo.id}
+                                onClick={async () => {
+                                    await updateTodoDone({ variables: { id: todo.id } })
+                                    await refetch()
+                                    // dispatch({ type: "toggleTodo", payload: 0 })
+                                }}
+                            >
+                                <Checkbox checked={todo.done} />
+                                <span>{todo.text}</span>
+                            </Flex>
+                        ))}
+                    </ul>
+                )}
             </Flex>
         </Container>
     )
