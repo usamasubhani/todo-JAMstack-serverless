8596b1133061f916e15e744affc60450ed9e3480
Usama Subhani <reachusama@protonmail.com>
2020-10-24 18:32:21 +0500

    Fix 'fetch' not found error

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,10 +1,12 @@
 const React = require("react");
 const { ApolloClient, HttpLink, ApolloProvider, InMemoryCache } = require("@apollo/client")
 const wrapRootElement = require("./wrap-root-element");
+const fetch = require('isomorphic-fetch')
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
+        fetch,
         uri: "https://todo-jamstack-serverless.netlify.app/.netlify/functions/graphql"
     })
 })
