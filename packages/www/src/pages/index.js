697d6c923ac83cdd22702168eac367b353d8cb58
Usama Subhani <reachusama@protonmail.com>
2020-10-24 14:03:57 +0500

    Install netlify-identity-widget for user authentication

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Button, Container, Heading, Flex } from "theme-ui"
+// import netlifyIdentity from "netlify-identity-widget"
+
+// netlifyIdentity.init({})
+
 export default props => (
     <Container>
         <Flex sx={{ flexDirection: "column", padding: 4 }}>
             <Heading>Hello World! </Heading>
             <Button onClick={() => {
-                alert("Clicked")
+                alert("CLICKED")
             }}>Login</Button>
         </Flex>
     </Container>
